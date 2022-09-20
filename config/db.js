b83f7d155539cee798f0e897b63b3a279e338627
MuhamadThoriqAhnaf <muhamad.t.a@mail.ugm.ac.id>
2022-09-20 11:41:32 +0700

    setup project, janlup npm i
    install monggose, morgan, express, cors, dotenv

---
+++ config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    const connection = await mongoose.connect('mongodb+srv://dbPAW:agnesiqbalfarrasfatin@cluster0.fgtvvu8.mongodb.net/?retryWrites=true&w=majority', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+
+    console.log(`MongoDB Connected: ${connection.connection.host}`);
+}; 
+
+module.exports = connectDB;

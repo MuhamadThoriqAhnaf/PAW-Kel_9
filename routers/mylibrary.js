b7898f96b414c70444707bd59599848f7a272f1c
Iqbalrez <iqbalrr02@gmail.com>
2022-09-23 20:54:14 +0700

    add Read

--- routers/mylibrary.js
+++ routers/mylibrary.js
@@ -7,4 +7,13 @@ router.route('/:id').get(getSingleBook);
 router.route('/').get(getAllBooks);
 
 
+// update data pada database
+router.put('../controllers/MyLibraryCrontoller/:id',function(req, res, next){
+    createBook.findByIdAndUpdate({_id: req.params.id}, req.body).then(function(){
+        createBook.findOne({_id: req.params.id}).then(function(book){
+            res.send(book)
+        });
+    });
+});
+
 module.exports=router
---
+++ routes/Books.routes.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+
+const{
+    removeBooks
+}=require('../controllers/delete.controllers')
+
+router.delete('/removebook/:id', removeBooks)
+module.exports=router   

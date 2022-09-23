72901f32f6ff3c104ead18013b065daaa9e6cbdd
mfarrasi <farras.ilhami@mail.ugm.ac.id>
2022-09-23 20:51:12 +0700

    Menambah Update untuk MyLibrary

--- routers/mylibrary.js
+++ routers/mylibrary.js
@@ -4,4 +4,13 @@ const router = express.Router();
 const {createBook} = require('../controllers/MyLibraryCrontoller');
 router.route('/').post(createBook);
 
+// update data pada database
+router.put('../controllers/MyLibraryCrontoller/:id',function(req, res, next){
+    createBook.findByIdAndUpdate({_id: req.params.id}, req.body).then(function(){
+        createBook.findOne({_id: req.params.id}).then(function(book){
+            res.send(book)
+        });
+    });
+});
+
 module.exports=router

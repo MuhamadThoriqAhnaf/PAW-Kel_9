55fd91b9564e959d9fdf08bedbf2f9452b5828e6
mfarrasi <farras.ilhami@mail.ugm.ac.id>
2022-09-25 00:42:15 +0700

    Menambah function update di router

--- routers/mylibrary.js
+++ routers/mylibrary.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router();
 
-const {createBook, getSingleBook, getAllBooks} = require('../controllers/MyLibraryController');
+const {createBook, updateBook, getSingleBook, getAllBooks} = require('../controllers/MyLibraryController');
 router.post('/', createBook);
+router.put('/:id', updateBook);
 router.get('/:id', getSingleBook);
 router.get('/', getAllBooks);
 

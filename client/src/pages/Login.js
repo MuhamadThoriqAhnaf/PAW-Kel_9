d89e4d05a78c70d0397aa3446f92a0458450cdf6
mfarrasi <79151971+mfarrasi@users.noreply.github.com>
2022-11-14 21:50:21 +0700

    fix sign up to login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-export default function SignUp() {
+export default function Login() {
   return (
     <form>
         <label for="username">Username: </label>
         <input type="text" id="username" name="username"></input><br></br>
         <label for="password">Passsword: </label>
         <input type="password" id="password" name="password"></input><br></br>
-        <input type="submit" value="Login"></input>
+        <a href='/adminpage'><input type="button" value="Masuk"></input></a>
 
     </form>
   )

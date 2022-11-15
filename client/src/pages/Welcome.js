a17873ab92f568084d474ee5ecde2730eddb3d57
Fatinabimanyu <fatinabimanyu@mail.ugm.ac.id>
2022-11-15 22:11:35 +0700

    Update Welcome.js
    Membuat welcome page dan melink dengan login page admin

--- client/src/pages/Welcome.js
+++ client/src/pages/Welcome.js
@@ -1,11 +1,13 @@
 import React from 'react'
-
+import Navbar from '../components/navbar'
+ 
 export default function Welcome() {
   return (
     <>
-        
-        <img src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350"></img>
-        <a href='/login'>Login</a>
+        <Navbar/>
+        <div className='mx-[140px] my-[15px]'>
+          <a href='/login'className=''>Login</a>
+        </div>
     </>
   )
 }

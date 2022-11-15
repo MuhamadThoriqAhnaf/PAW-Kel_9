7f5e270bd74dbe4df341694c7b549f8786ca3539
Iqbalrez <iqbalrr02@gmail.com>
2022-11-15 18:25:02 +0700

    feat: add Login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -2,13 +2,24 @@ import React from 'react'
 
 export default function Login() {
   return (
-    <form>
-        <label for="username">Username: </label>
-        <input type="text" id="username" name="username"></input><br></br>
-        <label for="password">Passsword: </label>
-        <input type="password" id="password" name="password"></input><br></br>
-        <a href='/adminpage'><input type="button" value="Masuk"></input></a>
-
-    </form>
+    <main class="bg-login bg-cover bg-center-bottom bg-opacity-50 font-rubik flex items-center justify-center h-screen">
+      <form>
+        <div class="flex items-center justify-center mb-4">
+          <img src="logo-black.svg" class="h-10 fill-black"></img>
+        </div>
+        <div class="flex items-center justify-center mb-12">
+          <h1 class="text-3xl">Selamat datang di <span class="font-bold">MyLibrary</span>!</h1>
+        </div>
+        <div class="text-xl bg-white w-100 p-10 rounded-xl border border-black">
+          <p class="font-bold flex items-center justify-center mb-8">Masuk dengan akun MyLibrary Anda</p>
+          <label for="username" >Username: </label>
+          <input type="text" id="username" name="username" class="w-full p-2 rounded bg-[#D9E5D6] border border-black mb-4"></input><br></br>
+          <label for="password">Passsword: </label>
+          <input type="password" id="password" name="password" class="w-full p-2 rounded bg-[#D9E5D6] border border-black mb-8"></input><br></br>
+          <a href='/adminpage' class="flex items-center justify-center"><button type="submit" class="bg-[#0B3C49] border border-black w-1/4 text-white font-medium py-2 rounded hover:bg-black transition-colors">Masuk</button></a>
+        </div>
+      </form>
+    </main>
+    
   )
 }

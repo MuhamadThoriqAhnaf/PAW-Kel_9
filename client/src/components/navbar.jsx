60e5f9d220c9bddccf34b954ba4a7c40f9b8a813
Iqbalrez <iqbalrr02@gmail.com>
2022-11-16 08:06:33 +0700

    refactor: change Logo source in Navbar

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import Logo from "../assets/Logo.svg"
 
 export default function Navbar() {
   return (
     <div className='flex px-[10px] md:px-[140px] md:py-[16px] items-center justify-between bg-[#D9E5D6] h-[80px]'>
-        <img src={Logo} className="w-[150px]"></img>
+        <img src="logo-black.svg" className="h-10"></img>
         <input placeholder='Cari Buku' className='border-2 h-[34px] w-[150px]' ></input>
     
     </div>

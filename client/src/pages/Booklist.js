82a205ac2c444c85c86dfc2c86c30dab10b25f9d
agnessuarna <agnessuarna@mail.ugm.ac.id>
2022-11-18 01:21:41 +0700

    fix: login auth

--- client/src/pages/Booklist.js
+++ client/src/pages/Booklist.js
@@ -1,29 +1,51 @@
-import React, { useState, useEffect } from 'react'
-import Navbar from '../components/navbar'
-import useFetch from '../hooks/useFetch';
-import axios from "axios"
+import React, { useState, useEffect } from "react";
+import Navbar from "../components/navbar";
+import useFetch from "../hooks/useFetch";
+import axios from "axios";
 
 export default function Booklist() {
   // const { data, loading, error } = useFetch("http://localhost:5000/api/book");
   // console.log(data)
-  const [data, setData] = useState([])
-  
+  const [data, setData] = useState([]);
+
   useEffect(() => {
     axios.get("http://localhost:5000/api/book/getbook").then((response) => {
       setData(response.data.data);
-      console.log(response.data.data)
-    })
+      console.log(response.data.data);
+    });
   }, []);
   return (
     <>
-    
-    <Navbar/>
-    <div>
-    {data.map((data) => (
-            data.judul
-          ))}
-    </div>
+      <Navbar />
+      <div className="grid grid-cols-3 gap-x-12 px-10 py-4">
+        {data.map((data) => {
+          return (
+            <div className="border border-black w-full p-4">
+              <h1>tanya sonia lanjutannya</h1>
+              <h1>tanya sonia lanjutannya</h1>
+              <h1>tanya sonia lanjutannya</h1>
+              <h1>tanya sonia lanjutannya</h1>
+              <h1>tanya sonia lanjutannya</h1>
+              <h1>tanya sonia lanjutannya</h1>
+              <h1>tanya sonia lanjutannya</h1>
+              <h1>tanya sonia lanjutannya</h1>
+              <h1>tanya sonia lanjutannya</h1>
+              <h1>tanya sonia lanjutannya</h1>
+              <h1>tanya sonia lanjutannya</h1>
+              <h1>tanya sonia lanjutannya</h1>
+              <h1>tanya sonia lanjutannya</h1>
+              <h1>tanya sonia lanjutannya</h1>
+              <p>{data.judul}</p>
+              <p>{data.penulis}</p>
+              <p>{data.terbit}</p>
+              <div className="flex gap-4">
+                <button className="bg-purple-500 py-2 px-3">Perbarui</button>
+                <button className="bg-red-500 py-2 px-3">Hapus</button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </>
-
-  )
+  );
 }
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,26 +1,69 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
 
 export default function Login() {
+  const test = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post(
+        "http://localhost:5000/api/auth/signin",
+        {
+          username: e.target[0].value,
+          password: e.target[1].value,
+        }
+      );
+
+      alert(`login berhasil xixixi. selamat datang ${data.username}`);
+    } catch (e) {
+      if (e.response) alert(e.response?.data?.message);
+      else alert("something went wrong");
+    }
+  };
+
   return (
     <main class="bg-login-phone sm:bg-login bg-cover bg-center font-rubik flex items-center justify-center h-screen">
-      <form>
+      <form onSubmit={test}>
         <div class="flex items-center justify-center mb-4">
           <img src="logo-black.svg" class="h-10 fill-black"></img>
         </div>
         <div class="flex items-center justify-center mb-12">
-          <h1 class="text-xl sm:text-3xl">Selamat datang di <span class="font-bold">MyLibrary</span>!</h1>
+          <h1 class="text-xl sm:text-3xl">
+            Selamat datang di <span class="font-bold">MyLibrary</span>!
+          </h1>
         </div>
         <div class="text-sm sm:text-xl bg-white w-100 p-8 sm:p-10 rounded-xl border border-black">
-          <p class="font-bold flex items-center justify-center mb-6 sm:mb-8">Masuk dengan akun MyLibrary Anda</p>
-          <label for="username" >Username: </label>
-          <input type="text" id="username" name="username" class="w-full p-2 rounded bg-[#D9E5D6] border border-black mb-4"></input><br></br>
+          <p class="font-bold flex items-center justify-center mb-6 sm:mb-8">
+            Masuk dengan akun MyLibrary Anda
+          </p>
+          <label for="username">Username: </label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            class="w-full p-2 rounded bg-[#D9E5D6] border border-black mb-4"
+          ></input>
+          <br></br>
           <label for="password">Passsword: </label>
-          <input type="password" id="password" name="password" class="w-full p-2 rounded bg-[#D9E5D6] border border-black mb-6 sm:mb-8"></input><br></br>
-          <a href='/adminpage' class="flex items-center justify-center"><button type="submit" class="bg-[#0B3C49] border border-black w-1/4 text-white font-medium py-2 rounded hover:bg-black transition-colors">Masuk</button></a>
-          <a href='/list'className=''>Booklist</a>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            class="w-full p-2 rounded bg-[#D9E5D6] border border-black mb-6 sm:mb-8"
+          ></input>
+          <br></br>
+          <a href="/adminpage" class="flex items-center justify-center">
+            <button
+              type="submit"
+              class="bg-[#0B3C49] border border-black w-1/4 text-white font-medium py-2 rounded hover:bg-black transition-colors"
+            >
+              Masuk
+            </button>
+          </a>
+          <a href="/list" className="">
+            Booklist
+          </a>
         </div>
       </form>
     </main>
-    
-  )
+  );
 }

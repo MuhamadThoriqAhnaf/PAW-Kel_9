2a90fe730fa884f2e611d083069d14501e6275ee
MuhamadThoriqAhnaf <muhamad.t.a@mail.ugm.ac.id>
2022-11-17 20:31:41 +0700

    navbar final dah kalo ga final fatin homo

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -7,7 +7,7 @@ export default function Navbar() {
         <img src="logo-black.svg " class="h-10 flex-auto"></img>
         <span class=" ml-2 mt-2 font-bold font-rubik text-xl flex-auto">MyLibrary</span>
       </div>
-        <input placeholder='Cari Buku...' className='placeholder:italic placeholder:text-slate-400 block bg-white md:w-full w-200 border border-slate-300 rounded-md py-2 pl-3 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm border-10 h-[34px] md:w-[300px]' ></input>
+        <input placeholder='Cari Buku...' className='placeholder:italic placeholder:text-slate-400 block bg-white md:w-full w-200 border border-slate-300 rounded-md py-2 pl-3 pr-3 shadow-sm focus:outline-none focus:border-black focus:ring-black focus:ring-1 sm:text-sm border-10 h-[34px] md:w-[300px]' ></input>
     </div>
   )
 }

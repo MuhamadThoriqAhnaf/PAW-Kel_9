c5a5b377c98e50c30e1be73fff3e8c208d738959
MuhamadThoriqAhnaf <muhamad.t.a@mail.ugm.ac.id>
2022-11-22 17:50:06 +0700

    add basic 404 page

---
+++ client/src/pages/NotFound.js
@@ -0,0 +1,7 @@
+const NotFound = () => {
+    return ( 
+        <div className="py-10 px-28 h-screen mx-auto mt-12 text-2xl font-bold">Sorry, you've found a page that doesnt exist 😞</div>
+     );
+}
+ 
+export default NotFound;

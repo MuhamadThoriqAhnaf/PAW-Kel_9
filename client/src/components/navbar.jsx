ec0935b9bbfdf42342085ae152f0d6bab72ec262
MuhamadThoriqAhnaf <muhamad.t.a@mail.ugm.ac.id>
2022-11-24 20:57:23 +0700

    navbar fix, kalo ga fix fatin normal

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 export default function Navbar() {
   return (
     <div className='rounded-b-3xl flex px-[10px] md:px-[140px] md:py-[16px] items-center justify-between bg-[#D9E5D6] h-[80px]'>
-      <div class="flex">
+      <a href="/" class="flex">
         <img src="logo-black.svg " class="h-10 flex-auto"></img>
         <span class=" ml-2 mt-2 font-bold font-rubik text-xl flex-auto">MyLibrary</span>
-      </div>
-      <div className="md:my-[20px]">
-        <a href="/login" className="md:pr-20 text-xl text-black font-bold font-rubik transition-colors hover:text-blue-800 ">
-          Login
         </a>
+      <div className="md:my-[20px]">
+        className="md:pr-20 text-xl text-black font-bold font-rubik transition-colors hover:text-blue-800 ">
+          Login
+        
       </div>
     </div>
   )

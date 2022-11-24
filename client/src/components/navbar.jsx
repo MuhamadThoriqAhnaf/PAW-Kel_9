e113ab99a26f06e02816cb497acdbba66460f3c9
Iqbalrez <iqbalrr02@gmail.com>
2022-11-25 00:32:51 +0700

    style: change padding and button style Navbar

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div className='rounded-b-3xl flex px-[10px] md:px-[140px] md:py-[16px] items-center justify-between bg-[#D9E5D6] h-[80px]'>
+    <div className='rounded-b-3xl flex px-10 md:px-20 md:py-[16px] items-center justify-between bg-[#D9E5D6] h-[80px]'>
       <a href="/" class="flex">
         <img src="logo-black.svg " class="h-10 flex-auto"></img>
-        <span class=" ml-2 mt-2 font-bold font-rubik text-xl flex-auto">MyLibrary</span>
+        <span class="ml-2 mt-2 font-bold font-rubik text-xl flex-auto">MyLibrary</span>
         </a>
         <a href="/login">
-        <button type="button" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-full text-lg px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Login</button>
+        <button type="button" class="bg-green border border-black text-white font-medium font-rubik px-4 py-2 rounded hover:bg-black transition-colors">Login</button>
         </a>
         </div>
 
--- client/src/components/navbar2.jsx
+++ client/src/components/navbar2.jsx
@@ -10,7 +10,7 @@ const signout = async (id) => {
 
 export default function Navbar2() {
   return (
-    <div className="rounded-b-3xl flex px-[10px] md:px-[140px] md:py-[16px] items-center justify-between bg-[#D9E5D6] h-[80px]">
+    <div className="rounded-b-3xl flex px-10 md:px-20 md:py-[16px] items-center justify-between bg-[#D9E5D6] h-[80px]">
       <a href="/" class="flex">
         <img src="logo-black.svg " class="h-10 flex-auto"></img>
         <span class=" ml-2 mt-2 font-bold font-rubik text-xl flex-auto">
@@ -21,8 +21,7 @@ export default function Navbar2() {
         <button
           type="button" 
           onClick={() => signout()}
-          class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-full text-lg px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
-        >
+          class="bg-green border border-black text-white font-medium font-rubik px-4 py-2 rounded hover:bg-black transition-colors">
           Logout
         </button>
       </a>

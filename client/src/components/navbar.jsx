8f9a636217664b6d84fb59453cfcce672d376db6
agnessuarna <agnessuarna@mail.ugm.ac.id>
2022-11-25 12:32:48 +0700

    fix : update navbar

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React from "react";
 
 export default function Navbar() {
   return (
-    <div className='rounded-b-3xl flex px-10 md:px-20 md:py-[16px] items-center justify-between bg-[#D9E5D6] h-[80px]'>
-      <a href="/" class="flex">
+    <div className="rounded-b-3xl flex px-10 md:px-20 md:py-[16px] items-center justify-between bg-[#D9E5D6] h-[80px]">
+      <a class="flex">
         <img src="logo-black.svg " class="h-10 flex-auto"></img>
-        <span class="ml-2 mt-2 font-bold font-rubik text-xl flex-auto">MyLibrary</span>
-        </a>
-        <a href="/login">
-        <button type="button" class="bg-green border border-black text-white font-medium font-rubik px-4 py-2 rounded hover:bg-black transition-colors">Login</button>
-        </a>
-        </div>
-
-  )
+        <span class="ml-2 mt-2 font-bold font-rubik text-xl flex-auto">
+          MyLibrary
+        </span>
+      </a>
+      <a href="/login">
+        <button
+          type="button"
+          class="bg-green border border-black text-white font-medium font-rubik px-4 py-2 rounded hover:bg-black transition-colors"
+        >
+          Login
+        </button>
+      </a>
+    </div>
+  );
 }
--- client/src/components/navbar2.jsx
+++ client/src/components/navbar2.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import axios from "axios";
 
 const signout = async (id) => {
-  await axios.post(`http://localhost:5000/api/auth/signout`)
+  await axios.post(`http://localhost:5000/api/auth/signout`);
   {
     localStorage.removeItem("accessToken");
-  };
+  }
 };
 
 export default function Navbar2() {
   return (
     <div className="rounded-b-3xl flex px-10 md:px-20 md:py-[16px] items-center justify-between bg-[#D9E5D6] h-[80px]">
-      <a href="/" class="flex">
+      <a class="flex">
         <img src="logo-black.svg " class="h-10 flex-auto"></img>
         <span class=" ml-2 mt-2 font-bold font-rubik text-xl flex-auto">
           MyLibrary
@@ -19,9 +19,10 @@ export default function Navbar2() {
       </a>
       <a href="/">
         <button
-          type="button" 
+          type="button"
           onClick={() => signout()}
-          class="bg-green border border-black text-white font-medium font-rubik px-4 py-2 rounded hover:bg-black transition-colors">
+          class="bg-green border border-black text-white font-medium font-rubik px-4 py-2 rounded hover:bg-black transition-colors"
+        >
           Logout
         </button>
       </a>

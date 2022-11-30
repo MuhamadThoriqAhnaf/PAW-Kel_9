0c627c601060df2881e3733cdf7b894d20dabd3f
agnessuarna <agnessuarna@mail.ugm.ac.id>
2022-11-30 12:16:48 +0700

    feat : notification on form

--- client/src/components/delete-form.jsx
+++ client/src/components/delete-form.jsx
@@ -3,14 +3,16 @@ import { useState, useEffect } from "react";
 import { storage } from "../firebase";
 import { ref, uploadBytes, listAll, getDownloadURL } from "firebase/storage";
 import axios from "axios";
-import 'flowbite';
+import "flowbite";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function DeleteForm({ data: initialData, setRefreshSignal }) {
   const [judul, setJudul] = useState(initialData?.judul);
   const [penulis, setPenulis] = useState(initialData?.penulis);
-  
+
   const [showUpdate, setShowUpdate] = React.useState(false);
-  
+
   const data = {
     judul: judul,
     penulis: penulis,
@@ -18,15 +20,16 @@ export default function DeleteForm({ data: initialData, setRefreshSignal }) {
 
   async function Delete(e) {
     e.preventDefault();
-    const res = await axios.delete(`http://localhost:5000/api/book/${initialData._id}`);
+    const res = await axios.delete(
+      `http://localhost:5000/api/book/${initialData._id}`
+    );
     setRefreshSignal((s) => !s);
     console.log(res);
-    
-    alert("Berhasil menghapus buku!");
-    setShowUpdate(false).then(
-    window.location.reload(true));
+
+    toast.success("Berhasil menghapus buku!");
+    setShowUpdate(false).then(window.location.reload(true));
   }
-  
+
   return (
     <div>
       <button
@@ -52,15 +55,15 @@ export default function DeleteForm({ data: initialData, setRefreshSignal }) {
               </div>
               <hr class="mb-4 h-px bg-black border-0"></hr>
               <form class="mb-8 grid grid-flow-row gap-4">
-                <div> Yakin menghapus {data.judul} oleh {data.penulis}?
-                  </div>
+                <div>
+                  {" "}
+                  Yakin menghapus {data.judul} oleh {data.penulis}?
+                </div>
               </form>
               <div class="flex justify-center">
                 <button
                   class="bg-pink border border-black break-words text-white font-medium text-sm sm:text-xl px-4 py-1 rounded hover:bg-black transition-colors"
-                  onClick={(e) =>
-                    Delete(e) + setShowUpdate(false)
-                  }
+                  onClick={(e) => Delete(e) + setShowUpdate(false)}
                 >
                   Hapus
                 </button>

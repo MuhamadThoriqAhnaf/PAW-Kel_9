830c303183feb6e9b00e59be28e04b802c684008
agnessuarna <agnessuarna@mail.ugm.ac.id>
2022-11-30 09:23:37 +0700

    fix : navbar2

--- client/src/components/navbar2.jsx
+++ client/src/components/navbar2.jsx
@@ -17,23 +17,25 @@ const signout = async (id) => {
 
 export default function Navbar2() {
   return (
-    <div className="rounded-b-3xl flex px-10 md:px-20 md:py-[16px] items-center justify-between bg-[#D9E5D6] h-[80px]">
+    <>
       <ToastContainer />
-      <a class="flex">
-        <img src="logo-black.svg " class="h-10 flex-auto"></img>
-        <span class=" ml-2 mt-2 font-bold font-rubik text-xl flex-auto">
-          MyLibrary
-        </span>
-      </a>
-      {/* <a href="/"> */}
-      <button
-        type="button"
-        onClick={() => signout()}
-        class="bg-green border border-black text-white font-medium font-rubik px-4 py-2 rounded hover:bg-black transition-colors"
-      >
-        Logout
-      </button>
-      {/* </a> */}
-    </div>
+      <div className="rounded-b-3xl flex px-10 md:px-20 md:py-[16px] items-center justify-between bg-[#D9E5D6] h-[80px]">
+        <a class="flex">
+          <img src="logo-black.svg " class="h-10 flex-auto"></img>
+          <span class=" ml-2 mt-2 font-bold font-rubik text-xl flex-auto">
+            MyLibrary
+          </span>
+        </a>
+        {/* <a href="/"> */}
+        <button
+          type="button"
+          onClick={() => signout()}
+          class="bg-green border border-black text-white font-medium font-rubik px-4 py-2 rounded hover:bg-black transition-colors"
+        >
+          Logout
+        </button>
+        {/* </a> */}
+      </div>
+    </>
   );
 }

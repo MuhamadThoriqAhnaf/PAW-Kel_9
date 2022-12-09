cf59f59a706ba6e6a25e2bd4aabf2b62b3f0c4cc
Iqbalrez <iqbalrr02@gmail.com>
2022-12-09 20:22:37 +0700

    fix: adjusting text

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -2,17 +2,17 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <div className="rounded-b-3xl flex px-10 md:px-20 md:py-[16px] items-center justify-between bg-[#D9E5D6] h-[80px]">
+    <div className="rounded-b-3xl flex px-10 md:px-20 md:py-[16px] items-center justify-between bg-tosca h-[80px]">
       <a class="flex">
-        <img src="logo-black.svg " class="h-10 flex-auto"></img>
-        <span class="ml-2 mt-2 font-bold font-rubik text-xl flex-auto">
+        <img src="logo-black.svg " class="h-8 sm:h-9 md:h-10 flex-auto"></img>
+        <span class="ml-2 mt-2 font-bold font-rubik text-md sm:text-lg md:text-xl flex-auto">
           MyLibrary
         </span>
       </a>
       <a href="/login">
         <button
           type="button"
-          class="bg-green border border-black text-white font-medium font-rubik px-4 py-2 rounded hover:bg-black transition-colors"
+          class="bg-green border border-black text-white font-medium text-xs sm:text-sm md:text-md font-rubik px-4 py-2 rounded hover:bg-black transition-colors"
         >
           Login
         </button>
